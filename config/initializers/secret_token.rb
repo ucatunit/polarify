b911f6eba45e484604baed53418fd05559deee1c
Marcin Pasternak <marcin.pasternak@copper.io>
2016-03-22 21:38:28 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polarify::Application.config.secret_token = '0dfc736526fb10a2a0b8f32abdd95da7e8109c054436b7fdf675a16ed1ee83f7476892b8bd72597a9aac101a44c6d9c3be6eaf1ef4b2776b38028e9991eefa15'
